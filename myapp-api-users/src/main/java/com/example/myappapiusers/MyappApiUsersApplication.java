71698c0ba6288a5c7bf86b1f611b80fbed9c6667
Ppojin <25076967+Ppojin@users.noreply.github.com>
2020-05-12 12:34:24 +0900

    [add] first app

---
+++ myapp-api-users/src/main/java/com/example/myappapiusers/MyappApiUsersApplication.java
@@ -0,0 +1,13 @@
+package com.example.myappapiusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappApiUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyappApiUsersApplication.class, args);
+    }
+
+}

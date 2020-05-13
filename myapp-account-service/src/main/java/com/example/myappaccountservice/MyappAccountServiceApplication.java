0bce01a18331c0eaf7b4696699f9241ce62776a5
Ppojin <25076967+Ppojin@users.noreply.github.com>
2020-05-13 12:06:24 +0900

    [mod] enable eureka on account service

--- myapp-account-service/src/main/java/com/example/myappaccountservice/MyappAccountServiceApplication.java
+++ myapp-account-service/src/main/java/com/example/myappaccountservice/MyappAccountServiceApplication.java
@@ -2,9 +2,10 @@ package com.example.myappaccountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class MyappAccountServiceApplication {
 
     public static void main(String[] args) {
